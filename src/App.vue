cc837e0b1ff6e70b366f6e9c3ae98ae27cd99185
elsaaaa1 <165806166+elsaaaa1@users.noreply.github.com>
2024-06-19 23:40:01 +0700

    UAS Teori PBK

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header class="header">
+      <q-toolbar>
+        <q-btn-group flat class="rout-nav">
+          <router-link to="/">
+            <q-btn flat :outline="!isActive('/')" label="Todos" />
+          </router-link>
+          <router-link to="/posts">
+            <q-btn flat :outline="!isActive('/posts')" label="Posts" />
+          </router-link>
+          <router-link to="/albums">
+            <q-btn flat :outline="!isActive('/albums')" label="Albums" />
+          </router-link>
+        </q-btn-group>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <q-page>
+        <router-view />
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { useRoute } from 'vue-router';
+
+export default {
+  setup() {
+    const route = useRoute();
+    const isActive = (path) => route.path === path;
+    return {
+      isActive
+    };
+  }
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: #ffc0cb !important;
+  color: #fff0f5 !important;
+  padding: 10px;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center; 
+}
+
+.rout-nav {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+.rout-nav button {
+  color: #d81b60;
+  font-size: 16px;
+}
+
+.rout-nav :hover {
+  color: whitesmoke;
+}
+
+.rout-nav a q-btn {
+  color: #ffc0cb;
+  font-size: 16px;
+}
+
+.rout-nav q-btn[flat][outline] {
+  border-color: #ffc0cb;
+  color: #ffc0cb;
+}
+</style>
